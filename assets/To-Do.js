122efd1fe15287e3b3d25831205d84a2d21162ad
Cecchy2 <dariocecchinato@gmail.com>
2024-05-15 16:03:10 +0200

    Sono riuscito con lista non ordinata a creare la lavagna

--- assets/To-Do.js
+++ assets/To-Do.js
@@ -5,30 +5,14 @@ form.addEventListener("submit", function (event) {
 
   const taskToDo = document.getElementById("inputId");
 
-  //---------------------------------------prima
-  /* console.log(taskToDo.value); */
-
-  /* const nuovaTask = {
-    task: taskToDo.value,
-  };
-
-  const lavagnaTask = document.getElementById("lavagna");
-  lavagnaTask.innerText = nuovaTask.task; */
-
-  //---------------------------------------seconda
-
-  /* const taskValue = taskToDo.value;
-  console.log(taskValue);
-  const lavagna = document.getElementById("lavagna");
-  lavagna.append(taskValue);
-  console.log(event);
-  taskToDo.value = ""; */
-
-  //--------------------------------------terza
-
   const taskValue = taskToDo.value;
 
   const lavagna = document.getElementById("lavagna");
 
-  lavagna.innerText = taskValue;
+  const liTaskValue = document.createElement("li");
+  liTaskValue.innerText = taskValue;
+
+  lavagna.appendChild(liTaskValue);
+
+  taskToDo.value = "";
 });
